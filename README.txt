af6ae3e5d06426f6d82ed85fb2ef519f4c04891b
Jack Cuthbertson <J.Cuthbertson1@newcastle.ac.uk>
2019-02-28 13:42:39 +0000

    This is the initial code added for this program. it includes a function that will replace certain codons in restriction sites with others and a function that will check through a sequence of DNA looking for restriction recognition sites. The readme file is also updated to include basic information of the functionality of the program.

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
-This is a placeholder readme file
+Restriction Site Remover
+
+This program reads a DNA sequence that is fed in as a FASTA file and will remove unwanted restriction sites. 
+This program can be used to produce BioBrick compatible sequences, but is also helpful with other DNA assembly methods that require restriction sites.
+
--- build.py
+++ build.py
@@ -1,5 +1,33 @@
 # imports
+from Bio import SeqIO
+from Bio.Seq import Seq
+
 
 # functions
+def replace_codon(input_restriction_site):
+    for i in range(0, len(input_restriction_site)):
+        checked_codon = input_restriction_site[i:i+3]
+        replaced_codon = input_restriction_site[i:i+3]
+        if checked_codon == "TTT":
+            replaced_codon = "TTC"
+        if checked_codon == "TTC":
+            replaced_codon = "TTT"
+        if input_restriction_site[i:i+3] != replaced_codon:
+            input_restriction_site = input_restriction_site.replace(checked_codon, replaced_codon)
+    return input_restriction_site
+
+
+def remove_restriction_sites(input_DNA):
+    for strand, nuc in [(+1, input_DNA)]:
+        for restriction_site in restriction_sites:
+            for i in range(len(nuc)):
+                r = len(restriction_site)
+                if nuc[i:i+r] == restriction_site:
+                    nuc = nuc[:i] + nuc[i:i+r].replace(nuc[i:i+r], replace_codon(nuc[i:i+r])) + nuc[i+r:]
+    return nuc
+
 
 # main
+restriction_sites = ["GAATTC"]
+
+print(remove_restriction_sites("AAGGCCTTAAGAATTCAA"))
