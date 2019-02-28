e461ddec8deb20b47b12b5eeab7620991ffc6618
Jack Cuthbertson <J.Cuthbertson1@newcastle.ac.uk>
2019-02-28 14:45:14 +0000

    New functionality that allows users to choose from 4 restrictions sites that they want removed from their DNA sequence. Readme updated to reflect this.

--- README.txt
+++ README.txt
@@ -3,3 +3,9 @@ Restriction Site Remover
 This program reads a DNA sequence that is fed in as a FASTA file and will remove unwanted restriction sites. 
 This program can be used to produce BioBrick compatible sequences, but is also helpful with other DNA assembly methods that require restriction sites.
 
+How to use.
+
+Run build.py with python3
+The program will ask you which restriction sites you wish to use.
+It has 4 restriction sites built in automatically, EcoR1, Xba1, Spe1, Pst1.
+
--- build.py
+++ build.py
@@ -4,6 +4,22 @@ from Bio.Seq import Seq
 
 
 # functions
+# this function will add restriction sites to be removed to a list based on user input
+def add_chosen_restriction_sites(chosen_site):
+    if chosen_site in ["1", "e", "E"]:
+        restriction_sites.append("GAATTC")
+    elif chosen_site in ["2", "x", "X"]:
+        restriction_sites.append("TCTAGA")
+    elif chosen_site in ["3", "s", "S"]:
+        restriction_sites.append("ACTAGT")
+    elif chosen_site is ["4", "p", "P"]:
+        restriction_sites.append("CTGCAG")
+    else:
+        print("Your input does not work please try again")
+
+
+# When a restriction site is found this function will  be called and pass through the site finding a codon it can change
+# while attempting to not change the amino acids expressed in the codon sequence
 def replace_codon(input_restriction_site):
     for i in range(0, len(input_restriction_site)):
         checked_codon = input_restriction_site[i:i+3]
@@ -17,6 +33,8 @@ def replace_codon(input_restriction_site):
     return input_restriction_site
 
 
+# This function will pass through a DNA sequence until it finds a restriction site. it will then call the replace_codon
+# function which will remove it. It then replaces the old site with the new one and continues searching.
 def remove_restriction_sites(input_DNA):
     for strand, nuc in [(+1, input_DNA)]:
         for restriction_site in restriction_sites:
@@ -28,6 +46,23 @@ def remove_restriction_sites(input_DNA):
 
 
 # main
-restriction_sites = ["GAATTC"]
+restriction_sites = []
+flag = True
+first_choice = input("What restriction sites would you like to remove from your DNA sequence? \n"
+                     "You can choose from 1) EcoR1, 2) Xba1, 3) Spe1 and 4) Pst1.\n"
+                     "If you would like to choose one of these enzymes enter the corresponding "
+                     "number or first initial\n")
+print(first_choice)
+add_chosen_restriction_sites(first_choice)
+while flag is True:
+    user_input = input("Would you like to add another restriction site? (Y/N)\n")
+    if user_input in ["Y", "Yes", "y", "yes"]:
+        add_chosen_restriction_sites(input("Which restriction site would you like to add?\n"
+                                           "1) EcoR1, 2) Xba1, 3) Spe1 and 4) Pst1\n"))
+    elif user_input in ["N", "No", "n", "no"]:
+        flag = False
+    else:
+        pass
 
-print(remove_restriction_sites("AAGGCCTTAAGAATTCAA"))
+print(restriction_sites)
+#print(remove_restriction_sites("AAGGCCTTAAGAATTCAA"))
