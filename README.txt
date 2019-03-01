e9ec63e2268f5ca177f1a584035a1cca48587a7b
Jack Cuthbertson <J.Cuthbertson1@newcastle.ac.uk>
2019-03-01 13:59:53 +0000

    The file will now take into account protein coding sequences when removing restriction sites to make sure that the amino acid sequence is not changed. Readme has been updated

--- README.txt
+++ README.txt
@@ -2,10 +2,12 @@ Restriction Site Remover
 
 This program reads a DNA sequence that is fed in as a FASTA file and will remove unwanted restriction sites. 
 This program can be used to produce BioBrick compatible sequences, but is also helpful with other DNA assembly methods that require restriction sites.
+Protein coding sequences are taken into account when using this program. Inside coding sequences the program will make sure that the bases that are changed will not change the amino acid sequence.
+Careful! Outside of protein coding sequences this program will not care which bases are changed. This means that promotors or ribosome binding sites etc. will possibly have function removed.
 
 How to use.
 
-Run build.py with python3
+Run build.py with python3. The program has been tested in the terminal and using pycharm professional edition.
 The program will ask you which restriction sites you wish to use.
 It has 4 restriction sites built in automatically, EcoR1, Xba1, Spe1, Pst1.
 If you wish to look for another restriction site please type in what the recognition site is.
--- build.py
+++ build.py
@@ -44,14 +44,22 @@ def add_chosen_restriction_sites(chosen_site):
 
 # When a restriction site is found this function will be called and pass through the site finding a codon it can change
 # while attempting to not change the amino acids expressed in the codon sequence
-def replace_codon(input_restriction_site):
-    for i in range(0, len(input_restriction_site)):
+def replace_codon(frame, input_restriction_site):
+    for i in range(frame, len(input_restriction_site), 3):
         checked_codon = input_restriction_site[i:i+3]
         replaced_codon = input_restriction_site[i:i+3]
         if checked_codon == "TTT":
             replaced_codon = "TTC"
         if checked_codon == "TTC":
             replaced_codon = "TTT"
+        if checked_codon == "ATT":
+            replaced_codon = "ATC"
+        if checked_codon == "ATC":
+            replaced_codon = "ATT"
+        if checked_codon == "AAT":
+            replaced_codon = "AAC"
+        if checked_codon == "AAC":
+            replaced_codon = "AAT"
         if input_restriction_site[i:i+3] != replaced_codon:
             input_restriction_site = input_restriction_site.replace(checked_codon, replaced_codon)
     return input_restriction_site
@@ -60,18 +68,41 @@ def replace_codon(input_restriction_site):
 # This function will pass through a DNA sequence until it finds a restriction site. it will then call the replace_codon
 # function which will remove it. It then replaces the old site with the new one and continues searching.
 def remove_restriction_sites(input_DNA):
+    restriction_sites_changed = 1
     for strand, nuc in [(+1, input_DNA)]:
         for restriction_site in restriction_sites:
+            x = 0
             for i in range(len(nuc)):
                 r = len(restriction_site)
+                stop_codons = ["TAA", "TAG", "TGA"]
+                if "ATG" in nuc[i:i+r] and x == 0:
+                    x = 1
+                    start_codon = nuc.index("ATG", i, i+r)
+                for stop_codon in stop_codons:
+                    if stop_codon in nuc[i:i+r] and x == 1:
+                        if (nuc.index(stop_codon, i, i+r) - start_codon) % 3 == 0:
+                            x = 0
                 if nuc[i:i+r] == restriction_site:
-                    nuc = nuc[:i] + nuc[i:i+r].replace(nuc[i:i+r], replace_codon(nuc[i:i+r])) + nuc[i+r:]
+                    if x == 1:
+                        print(str(restriction_sites_changed) + " : " + str(nuc[i:i+r]) + "->" +
+                              nuc[i:i+r].replace(nuc[i:i+r], replace_codon((i-start_codon) % 3, nuc[i:i+r])) +
+                              " at position {} to {}".format(i, i+r))
+                        nuc = nuc[:i] + nuc[i:i+r].replace(nuc[i:i+r], replace_codon((i-start_codon) % 3, nuc[i:i+r])) \
+                            + nuc[i+r:]
+                        restriction_sites_changed += 1
+                    else:
+                        print(str(restriction_sites_changed) + " : " + str(nuc[i:i+r]) + "->" +
+                              nuc[i:i+r].replace(nuc[i:i+r], replace_codon(0, nuc[i:i+r])) +
+                              " at position {} to {}".format(i, i+r))
+                        nuc = nuc[:i] + nuc[i:i+r].replace(nuc[i:i+r], replace_codon(0, nuc[i:i+r])) + nuc[i+r:]
+                        restriction_sites_changed += 1
     return nuc
 
 
 # main
 # this is an empty list that will contain restriction sites that need to be changed.
 restriction_sites = []
+changed_restriction_sites = {}
 # Here the user is presented with options that will determine which restriction sites are placed into the list
 flag = True
 first_choice = input("What restriction sites would you like to remove from your DNA sequence? \n"
@@ -79,8 +110,9 @@ first_choice = input("What restriction sites would you like to remove from your 
                      "If you would like to choose one of these enzymes enter the corresponding "
                      "number or first initial\n"
                      "If you would prefer a different restriction enzyme please enter the recognition sequence.\n")
-print(first_choice)
+
 add_chosen_restriction_sites(first_choice)
+
 while flag is True:
     user_input = input("Would you like to add another restriction site? (Y/N)\n")
     if user_input in ["Y", "Yes", "y", "yes"]:
@@ -92,5 +124,6 @@ while flag is True:
         print("Sorry your input did not work please try again.")
         pass
 
-print(restriction_sites)
-#print(remove_restriction_sites("AAGGCCTTAAGAATTCAA"))
+print("Your chosen restriction sites are " + str(restriction_sites))
+print("AAAGAATTCGGGATGAGAATTCAAGGTTAGTGAATTC")
+print(remove_restriction_sites("AAAGAATTCGGGATGAGAATTCAAGGTTAGTGAATTC"))
