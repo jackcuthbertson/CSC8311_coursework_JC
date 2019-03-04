e1089d0f121eeb9c290d2b31a27b3925af826bfe
Jack Cuthbertson <J.Cuthbertson1@newcastle.ac.uk>
2019-03-04 16:09:12 +0000

    Added more tests to coursework_test.py

--- coursework_test.py
+++ coursework_test.py
@@ -1,7 +1,11 @@
 from build import correct_letters
 from build import seq_to_string
+from build import replace_codon
+from build import remove_restriction_sites
+from build import add_chosen_restriction_sites
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
+from Bio import SeqIO
 import unittest
 
 
@@ -14,8 +18,20 @@ class TestCoursework(unittest.TestCase):
         self.assertFalse(correct_letters("THFB"))
 
     def test_seq_to_string(self):
-        self.assertTrue(isinstance(SeqRecord(Seq(self.seq)), SeqRecord))
-        self.assertTrue(isinstance(seq_to_string(SeqRecord(Seq(self.seq))), str))
+        self.assertTrue(isinstance(seq_to_string(SeqIO.index("test.seq", "fasta")), str))
+
+    def test_replace_codon(self):
+        self.assertEqual(replace_codon(0, "TTC"), "TTT")
+        self.assertEqual(replace_codon(1, "ATTC"), "ATTT")
+
+    def test_add_chosen_restriction_sites(self):
+        restriction_sites = []
+        add_chosen_restriction_sites("1")
+        self.assertEqual(restriction_sites, ["GAATTC"])
+
+    def test_remove_restriction_sites(self):
+        add_chosen_restriction_sites("1")
+        self.assertNotEqual(remove_restriction_sites("GAATTC"), "GAATTC")
 
 
 if __name__ == '__main__':
